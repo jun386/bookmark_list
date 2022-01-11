76e264fdf373b4ca33d2781d053dafbdad9b43d2
jun386 <aka6116.aka@gmail.com>
2022-01-11 20:26:49 +0900

    bookmark 新規投稿機能追加

--- app/controllers/api/bookmarks_controller.rb
+++ app/controllers/api/bookmarks_controller.rb
@@ -1,8 +1,25 @@
 class Api::BookmarksController < ApplicationController
 
+  protect_from_forgery :except => [:create, :update, :destroy]
+
   def index
     @bookmarks = Bookmark.order('created_at DESC')
     render "index", formats: :json, handlers: "jbuilder"
   end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      head :no_content
+    else
+      render json: @bookmark.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+    def bookmark_params
+      params.require(:bookmark).permit(:title, :url, :category)
+    end
 
 end
