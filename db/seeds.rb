b63c395ec520488ef10505e08658508109b49def
jun386 <aka6116.aka@gmail.com>
2022-01-30 15:26:03 +0900

    :sparkles: userモデル　ダミーデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create!(
+  name: "test1",
+  email: "test1@example.com",
+  password: "password"
+)
+
+User.create!(
+  name: "test2",
+  email: "test2@example.com",
+  password: "password"
+)
+
 10.times do
   Bookmark.create(
     title: Faker::Lorem.word,
     url: Faker::Internet.url,
     category: Faker::Lorem.word,
+    user_id: 1
   )
 end
