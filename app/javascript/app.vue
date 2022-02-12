3bbe3b3ff5d5410c2841b80af230fc871e12b8bc
jun386 <aka6116.aka@gmail.com>
2022-02-12 16:46:47 +0900

    :sparkles: サイドバー　コンポーネント化

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -32,18 +32,7 @@
           </div>
         </v-flex>
 
-        <v-flex xs2>
-          <v-btn @click="togglePostModal()" style="margin: 20px 0 40px 0;">
-            <span class="material-icons" style="margin-right: 4px;">add</span>Bookmarkを追加する
-          </v-btn>
-          <p style="margin-right: 30px">- Bookmark List -</p>
-          <ul v-for="bookmark in bookmarkList" :key="bookmark.id" style="list-style: none; margin-right: 30px">
-            <li style="margin-top: 10px;">
-              <a v-bind:href="bookmark.url" target="_blank" rel="noopener noreferrer">{{ bookmark.title }}</a>
-            </li>
-            <hr>
-          </ul>
-        </v-flex>
+        <Sidebar :bookmarkList="bookmarkList" @add-click="togglePostModal"></Sidebar>
       </v-layout>
 
         <AddDialog :dialogPostFlag="dialogPostFlag" :categoriesForEdit="categoriesForEdit" @post-cancel-click="togglePostModal" @post-click="postBookmark"></AddDialog>
@@ -81,6 +70,7 @@
 <script>
 import Loading from './components/Loading'
 import BookmarkList from './components/BookmarkList'
+import Sidebar from './components/Sidebar'
 import AddDialog from './components/AddDialog'
 import DeleteDialog from './components/DeleteDialog'
 import axios from 'axios';
@@ -125,6 +115,7 @@ export default {
   components: {
     Loading,
     BookmarkList,
+    Sidebar,
     AddDialog,
     DeleteDialog,
   },
---
+++ app/javascript/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-flex xs2>
+    <v-btn @click="togglePostModal()" style="margin: 20px 0 40px 0;">
+      <span class="material-icons" style="margin-right: 4px;">add</span>Bookmarkを追加する
+    </v-btn>
+    <p style="margin-right: 30px">- Bookmark List -</p>
+    <ul v-for="bookmark in bookmarkList" :key="bookmark.id" style="list-style: none; margin-right: 30px">
+      <li style="margin-top: 10px;">
+        <a v-bind:href="bookmark.url" target="_blank" rel="noopener noreferrer">{{ bookmark.title }}</a>
+      </li>
+      <hr>
+    </ul>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  props: ['bookmarkList'],
+  methods: {
+    togglePostModal() {
+      this.$emit('add-click')
+    }
+  }
+}
+</script>
